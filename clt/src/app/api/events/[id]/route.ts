1b192a80fe979de2297f9bd9d0c166319e360363
Stone Liu <sliu@spillcenter.com>
2024-08-23 13:07:34 -0500

    Some more stuff on the endpoints

--- clt/src/app/api/events/[id]/route.ts
+++ clt/src/app/api/events/[id]/route.ts
@@ -1,13 +1,19 @@
+import { connectDB, closeDB } from "@/lib/db/db"
 import { auth } from "@/lib/utils/auth/auth"
-
+import EventModel from "@/lib/db/models/event"
 export async function PATCH(req: Request) {
   const session = await auth()
   if (!session)
     return Response.json({ message: "unauthorized" }, { status: 401 })
   try {
-    const body = req.json()
-    console.log(body)
+    const body = await req.json()
+    const id = body.eventId
+    //initialize a connection to the database
+    await connectDB()
+    await EventModel.findByIdAndUpdate(id, {})
+    await closeDB()
+    return Response.json({ message: "Successful" }, { status: 200 })
   } catch (e) {
-    console.log(e)
+    return Response.json({ message: "Server Error" }, { status: 500 })
   }
 }
