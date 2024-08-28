e00320498cbcb03ab59a57a0ebefb91ae9faaad3
Stone Liu <sliu@spillcenter.com>
2024-08-27 22:39:32 -0500

    Fixed faulty google callback

---
+++ clt/src/app/api/announcements/route.ts
@@ -0,0 +1,44 @@
+import { closeDB, connectDB } from "@/lib/db/db"
+import AnnouncementModel from "@/lib/db/models/Announcement"
+import { auth } from "@/lib/utils/auth/auth"
+import { base64ToFile } from "@/lib/utils/file"
+import { uploadS3 } from "@/lib/utils/s3"
+import { AnnouncementSchema } from "@/types/IAnnouncement"
+
+export async function POST(req: Request) {
+  const session = await auth()
+  if (!session)
+    return Response.json({ message: "Unauthorized" }, { status: 401 })
+  const body = await req.json()
+  const zAnnouncement = AnnouncementSchema.safeParse(body)
+  if (!zAnnouncement.success) {
+    const error = zAnnouncement.error.format()
+    return Response.json({ error: error }, { status: 400 })
+  }
+  const announcement = zAnnouncement.data
+  if (announcement.announcementPhoto) {
+    const f = announcement.announcementPhoto
+    const removePrefix64 = f.base64.split(",")[1]
+    const file = base64ToFile(removePrefix64, f.fileName, f.fileType)
+    const link = await uploadS3(file)
+    Object.defineProperty(announcement, "announcementPhoto", {
+      value: { src: link, alt: f.fileName },
+      enumerable: true,
+    })
+  }
+  try {
+    await connectDB()
+    await AnnouncementModel.create(event)
+    await closeDB()
+    return new Response(
+      JSON.stringify({ message: "Announcement created successfully" }),
+      { status: 201 }
+    )
+  } catch (e: any) {
+    console.error(e)
+    return new Response(
+      JSON.stringify({ message: "Failed to create event", error: e.message }),
+      { status: 500 }
+    )
+  }
+}
--- clt/src/lib/db/models/Announcement.ts
+++ clt/src/lib/db/models/Announcement.ts
@@ -1,1 +1,9 @@
 import mongoose from "mongoose"
+
+const AnnouncementSchema = new mongoose.Schema({
+
+})
+
+const AnnouncementModel = mongoose.models.Announcement || mongoose.model("Announcement", AnnouncementSchema)
+
+export default AnnouncementModel
